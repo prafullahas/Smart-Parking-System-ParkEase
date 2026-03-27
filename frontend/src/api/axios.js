1d93ec123276eae375b371c7e9bc3318a3dc519b
Tejshree16 <tejshreedixit@gmail.com>
2026-03-27 20:00:17 +0530

    Updated backend API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an axios instance
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://smart-parking-backend-vl9t.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
